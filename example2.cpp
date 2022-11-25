a76596922bfd1398bf903c8527b762de9a145768
ryan597 <ryan.smith@ucdconnect.ie>
2022-11-25 23:05:50 +0000

    first commit

---
+++ example2.cpp
@@ -0,0 +1,73 @@
+// Include standard headers
+#include <iostream>
+#include <cstdlib>
+
+// Include GLEW. Always include it before gl.h and glfw3.h, since it's a bit magic.
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+
+// Include GLM
+#include <glm/glm.hpp>
+using namespace glm;
+
+auto main() -> int
+{
+    // Initialise GLFW
+    glewExperimental = true; // Needed for core profile
+    if( !glfwInit() )
+    {
+        std::cout << "Failed to initialize GLFW\n";
+        std::getchar();
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
+
+    // Open a window and create its OpenGL context
+    GLFWwindow* window;
+    window = glfwCreateWindow( 1024, 768, "Example Window", nullptr, nullptr);
+    if( window == nullptr ){
+        std::cout << "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible.\n";
+        std::getchar();
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        std::cout <<"Failed to initialize GLEW\n";
+        std::getchar();
+        glfwTerminate();
+        return -1;
+    }
+
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    // Dark blue background
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+    do{
+        // Clear the screen
+        glClear( GL_COLOR_BUFFER_BIT );
+
+        // Draw
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    } // Check if the ESC key was pressed or the window was closed
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+           glfwWindowShouldClose(window) == 0 );
+
+    glfwTerminate();
+    return 0;
+}
---
+++ shader.h
@@ -0,0 +1,3 @@
+#pragma once
+
+auto LoadShaders(const char* vertex_file_path, const char* fragment_file_path) -> GLuint;
